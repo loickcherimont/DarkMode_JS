0b3d46ad043458ce60b99e5df1eacb0e7684b1b4
LoickDev97438 <loickcherimont@gmail.com>
2022-10-06 21:17:37 +0200

    Tidy the dark mode switcher project

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # DarkMode_JS
-Dark Mode made with HTML, CSS and JavaScript
+
+## Introduction
+Light / Dark Mode switcher
+
+## Technologies
+- HTML
+- CSS
+- JavaScript
+
+## How to run
+### Prerequisite
+- Recent browser
+- Workspace
+- Git
+- CLI (Command Line Interface)
+
+1. Open your **workspace** :
+```bash
+cd WORKSPACE
+```
+
+2. Launch your **CLI** in this workspace :
+```bash
+git clone https://www.github.com/loickcherimont/DarkMode_JS
+```
+
+3. Go into the new created folder :
+```bash
+cd DarkMode_JS
+start index.html
+```
+4. You are on the main page of the feature!
+
+## License
+Made by Loick Cherimont, under MIT License
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 const switcher = document.getElementById("switcher");
-const curtains = document.querySelector(".curtains");
+const darkMode = document.querySelector(".dark-mode");
 
 switcher.addEventListener('click', () => {
 	const body = document.body;
@@ -9,12 +9,12 @@ switcher.addEventListener('click', () => {
 
 	// Dark mode is running
 	if(body.classList.contains("dark")) {
-		curtains.style.height = "100vh";
+		darkMode.style.height = "100vh";
 		switcher.value = "Light";
 		document.querySelector(".mode-indicator").innerHTML = '<i class="bi bi-moon-fill"></i>';
 	} else {
 		switcher.value = "Dark";
-		curtains.style.height = "0vh";
+		darkMode.style.height = "0vh";
 		document.querySelector(".mode-indicator").innerHTML = '<i class="bi bi-brightness-high-fill"></i>';
 	}1
 })
