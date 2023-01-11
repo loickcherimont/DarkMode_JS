c760099e783cc419b1e86ab049de73a413d549a1
LoickDev97438 <loickcherimont@gmail.com>
2023-01-11 22:28:06 +0100

    Main page of the app move all functions in a specific file

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,6 @@
-const switcher = document.getElementById("switcher");
-const darkMode = document.querySelector(".dark-mode");
+import { switcher } from "./functions.js";
 
-switcher.addEventListener('click', () => {
-	const body = document.body;
-	
+document
+	.getElementById("modeButton")
+	.addEventListener('click', switcher)
 
-	body.classList.toggle("dark");
-
-	// Dark mode is running
-	if(body.classList.contains("dark")) {
-		darkMode.style.height = "100vh";
-		switcher.value = "Light";
-		document.querySelector(".mode-indicator").innerHTML = '<i class="bi bi-moon-fill"></i>';
-	} else {
-		switcher.value = "Dark";
-		darkMode.style.height = "0vh";
-		document.querySelector(".mode-indicator").innerHTML = '<i class="bi bi-brightness-high-fill"></i>';
-	}1
-})
